d9a5d6157ecf40c07c4be441cbc2e97e9a570dcc
runAlgo <150834861+runAlgo@users.noreply.github.com>
2025-06-24 17:14:00 +0545

    implement hero scroll animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,40 +1,69 @@
 import { useGSAP } from "@gsap/react";
 import { SplitText } from "gsap/all";
 import gsap from "gsap";
+import { useRef } from "react";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
-  useGSAP(() => {
-    const heroSplit = new SplitText(".title", {type: "chars words"});
-    const paragraphSplit = new SplitText(".subtitle", {type: "lines"});
+  const videoRef = useRef();
 
-    heroSplit.chars.forEach((char) => char.classList.add('text-gradient'));
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+
+  useGSAP(() => {
+    const heroSplit = new SplitText(".title", { type: "chars words" });
+    const paragraphSplit = new SplitText(".subtitle", { type: "lines" });
+
+    heroSplit.chars.forEach((char) => char.classList.add("text-gradient"));
 
     gsap.from(heroSplit.chars, {
-        yPercent: 100,
-        duration: 1.8,
-        ease: 'expo.out',
-        stagger: 0.06
+      yPercent: 100,
+      duration: 1.8,
+      ease: "expo.out",
+      stagger: 0.06,
     });
 
     gsap.from(paragraphSplit.lines, {
-        opacity: 0,
-        yPercent: 100,
-        duration: 1.8,
-        ease: 'expo.out',
-        stagger: 0.06,
-        delay: 1,
+      opacity: 0,
+      yPercent: 100,
+      duration: 1.8,
+      ease: "expo.out",
+      stagger: 0.06,
+      delay: 1,
     });
 
-    gsap.timeline({
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: "#hero",
+          start: "top top",
+          end: "bottom top",
+          scrub: true,
+        },
+      })
+      .to(".right-leaf", { y: 200 }, 0)
+      .to(".left-leaf", { y: -200 }, 0);
+
+    const startValue = isMobile ? "top 50%" : "center 60%";
+    const endValue = isMobile ? "120% top" : "bottom top";
+
+    // Video animation timeline
+    // Create the timeline with a default duration
+
+    const tl = gsap.timeline({
       scrollTrigger: {
-        trigger: '#hero',
-        start: 'top top',
-        end: 'bottom top',
+        trigger: "video",
+        start: startValue,
+        end: endValue,
         scrub: true,
-      }
-    })
-    .to('.right-leaf', { y: 200 }, 0)
-    .to('.left-leaf', {y: -200 }, 0)
+        pin: true,
+      },
+    });
+
+    videoRef.current.onloadedmetadata = () => {
+      tl.to(videoRef.current, {
+        currentTime: videoRef.current.duration,
+      });
+    };
   }, []);
   return (
     <>
@@ -75,6 +104,16 @@ const Hero = () => {
           </div>
         </div>
       </section>
+
+      <div className="video absolute inset-0">
+        <video
+          src="/videos/output.mp4"
+          ref={videoRef}
+          muted
+          playsInline
+          preload="auto"
+        ></video>
+      </div>
     </>
   );
 };
